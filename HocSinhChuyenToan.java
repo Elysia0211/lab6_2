042aff3a7586953714e7701253ebedb0634e944e
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-03-19 12:22:50 +0700

    Create HocSinhChuyenToan.java

---
+++ HocSinhChuyenToan.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab6_2;
+
+/**
+ *
+ * @author Admin
+ */
+public class HocSinhChuyenToan extends HocSinh{
+
+    public HocSinhChuyenToan() {
+    }
+
+    public HocSinhChuyenToan(String HoTen, String Lop, float diemToan, float diemLy, float diemHoa) {
+        super(HoTen, Lop, diemToan, diemLy, diemHoa);
+    }
+    
+
+    @Override
+    public void diemTrungBinh() {
+        super.diemTrungBinh(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
