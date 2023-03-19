8cdf84cdc9130798007c198ece6d9a419d688e16
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-03-19 12:23:33 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab6_2;
+
+/**
+ *
+ * @author Admin
+ */
+public class Lab6_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HocSinh hs = new HocSinh();
+        hs.diemTrungBinh();
+        HocSinhChuyenToan hsct = new HocSinhChuyenToan();
+        hsct.diemTrungBinh();
+       
+    }
+    
+}
